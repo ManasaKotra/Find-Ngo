b9fd65e0607897775e8860a6f7cb2b81527ff210
manasakotra <kotramanasa@gmail.com>
2015-05-20 11:55:47 +0530

    search & display complete

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var bodyParser = require('body-parser');
 var connect = require('connect');
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost:27017/ngos'); 
+var db = mongoose.connect('mongodb://localhost:27017/ngos');
 var express = require('express');
 var app = express();
 
@@ -11,7 +11,7 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-// index page 
+// index page
 app.get('/', function(req, res) {
     res.render('index');
 });
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 //MongoDB setup
-var ngoSchema = new mongoose.Schema({  
+var ngoSchema = new mongoose.Schema({
   title: String,
   address: String,
   description: String,
@@ -35,36 +35,15 @@ var ngo = mongoose.model('ngo', ngoSchema);
 //search request
 app.post('/',function(req, res){
 
-	var req_locality = req.body.locality;
-	var req_interest = req.body.interest;
+	var req_locality = req.body.locality.toLowerCase();
+	var req_interest = req.body.interest.toLowerCase();
 
 	console.log(req.body);
 
-	
+
 	ngo.find( { "locality": req_locality } , function(err, docs) {
   		if (err) return console.log(err);
-  			// console.dir(doc);
-        // res.render('ngo', { docs: doc });
-
-        // for (var i = 0; i < docs.length; i++) {          
-        //     var title_doc = docs[i].title;
-        //     var address_doc = docs[i].address;
-        //     var description_doc = docs[i].description;
-        //     var contact_doc = docs[i].contact;
-
-        //     res.render('ngo', {title: title_doc, address: address_doc, 
-        //     description: description_doc, contact: contact_doc });
-        // }
-        
-        var ngoslist = {};
-
-        docs.forEach(function(ngo){
-          ngoslist[ngo.title] = ngo;
-        });
-
-        res.render('ngo', { ngolist: ngoslist } );
-
-
+        res.render('ngo', { list: docs });
 	});
 
 
@@ -76,18 +55,11 @@ app.post('ngo',function(req, res){
   var req_interest = req.body.interest;
 
   console.log(req.body);
-  
-  ngo.find( { "locality": req_locality } , function(err, doc) {
-      if (err) return console.log(err);
-        var ngoslist = {};
 
-        docs.forEach(function(ngo){
-          ngoslist[ngo.title] = ngo;
-        });
-
-        res.send(ngoslist);      
-
-  });
+  ngo.find( { "locality": req_locality } , function(err, docs) {
+  		if (err) return console.log(err);
+        res.render('ngo', { list: docs });
+	});
 
 
 });
