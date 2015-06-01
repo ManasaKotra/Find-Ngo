fb6dbc1d711297ecee477ce621f0fa95077c980c
manasakotra <kotramanasa@gmail.com>
2015-06-01 11:56:26 +0530

    complete gulpfile for build

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,51 @@
 // Gulp Dependencies
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var clean = require('gulp-clean');
+var copy = require('gulp-copy');
+var minifyCss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 
 // Test Dependencies
 var mochaPhantomjs = require('gulp-mocha-phantomjs');
+
+gulp.task('clean', function () {
+  return gulp.src('build/*', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('copy', function(){
+  gulp.src(['server.js'])
+    .pipe(gulp.dest('build/'));
+  gulp.src('views/**/*')
+    .pipe(gulp.dest("build/views"));
+  gulp.src(['public/fonts/*'])
+    .pipe(gulp.dest('build/public/fonts'));
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('public/stylesheets/*')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('build/public/stylesheets'));
+});
+
+gulp.task('minify-img', function () {
+    return gulp.src('public/images/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('build/public/images'));
+});
+
+gulp.task('minify-js', function() {
+  return gulp.src('public/javascripts/*')
+    .pipe(uglify())
+    .pipe(gulp.dest('build/public/javascripts'));
+});
 
 gulp.task('lint', function() {
   return gulp.src('*.js')
@@ -20,4 +62,5 @@ gulp.task('watch', function() {
     });
 });
 
+gulp.task('build', ['clean', 'copy', 'minify-css', 'minify-img', 'minify-js']);
 gulp.task('default', ['lint', 'watch']);
